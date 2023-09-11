41f18449e20fae9ad0178c6bbcecdfece021306d
Guilherme Daniel <gui.dani13@gmail.com>
2023-09-11 09:55:52 -0300

    Adicionar arquivos de projeto.

---
+++ NorthWindAPP/Default.aspx.designer.vb
@@ -0,0 +1,114 @@
+﻿'------------------------------------------------------------------------------
+' <gerado automaticamente>
+'     Este código foi gerado por uma ferramenta.
+'
+'     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+'     o código for recriado
+' </gerado automaticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class _Default
+
+    '''<summary>
+    '''Controle UpdatePanel1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents UpdatePanel1 As Global.System.Web.UI.UpdatePanel
+
+    '''<summary>
+    '''Controle Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Controle CheckBoxList1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents CheckBoxList1 As Global.System.Web.UI.WebControls.CheckBoxList
+
+    '''<summary>
+    '''Controle BtnToPause.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents BtnToPause As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle BtnClearList.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents BtnClearList As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle BtnRemoveMarked.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents BtnRemoveMarked As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle TextBox1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents TextBox1 As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Controle Button1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle BtnSelectAll.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents BtnSelectAll As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Controle GridView1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents GridView1 As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Controle SqlDataSource1.
+    '''</summary>
+    '''<remarks>
+    '''Campo gerado automaticamente.
+    '''Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+    '''</remarks>
+    Protected WithEvents SqlDataSource1 As Global.System.Web.UI.WebControls.SqlDataSource
+End Class
---
+++ NorthWindAPP/Default.aspx.vb
@@ -0,0 +1,87 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs)
+        'Dim texto = TextBox1.Text
+        'Dim ds = SqlDataSource1.SelectCommand = $"SELECT * FROM [NorthWindDb].[dbo].[Customers] WHERE ContactName LIKE '%{texto}%'"
+        'GridView1.DataBind()
+    End Sub
+
+    Protected Sub GridView1_RowCommand(sender As Object, e As GridViewCommandEventArgs)
+        If e.CommandName = "Select" Then
+            Dim index = Convert.ToInt32(e.CommandArgument)
+            Dim selectedRow As GridViewRow = GridView1.Rows(index)
+            Dim customerID As TableCell = selectedRow.Cells(3)
+            Dim id As String = customerID.Text
+
+            Dim listItem As New ListItem(id)
+
+            If Not CheckBoxList1.Items.Contains(listItem) Then
+                CheckBoxList1.Items.Add(listItem)
+            End If
+
+            'MsgBox($"Você selecionou: {id}")
+        End If
+    End Sub
+
+    Protected Sub GridView1_SelectedIndexChanged(sender As Object, e As EventArgs)
+        Dim item = GridView1.SelectedValue.ToString()
+        'MsgBox(item)
+    End Sub
+
+    Protected Sub CheckBoxList1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub BtnSelectAll_Click(sender As Object, e As EventArgs)
+        Dim rows As GridViewRowCollection = GridView1.Rows
+        For Each row As GridViewRow In rows
+            Dim customerID As TableCell = row.Cells(3)
+            Dim id As String = customerID.Text
+
+            Dim listItem As New ListItem(id)
+
+            If Not CheckBoxList1.Items.Contains(listItem) Then
+                CheckBoxList1.Items.Add(listItem)
+            End If
+        Next
+    End Sub
+
+    Protected Sub BtnToPause_Click(sender As Object, e As EventArgs)
+        Dim chcItems = CheckBoxList1.Items
+        For Each item As ListItem In chcItems
+            Dim a = item.Selected
+            Dim b = item.Value
+            Dim ee = item.Enabled
+            Dim f = item.ToString
+
+        Next
+    End Sub
+
+    Protected Sub BtnClearList_Click(sender As Object, e As EventArgs)
+        If MsgBox("Tem certeza que deseja remover todos os items da lista?", MsgBoxStyle.OkCancel) Then
+            CheckBoxList1.Items.Clear()
+        End If
+    End Sub
+
+    Protected Sub BtnRemoveMarked_Click(sender As Object, e As EventArgs)
+        Dim chcItems = CheckBoxList1.Items
+        Dim chcItemsCopy As New ListItemCollection
+        If MsgBox("Tem certeza que deseja remover todos os items selecionados?", MsgBoxStyle.OkCancel) Then
+            For Each item As ListItem In chcItems
+                If Not item.Selected Then
+                    chcItemsCopy.Add(item)
+                End If
+            Next
+            CheckBoxList1.Items.Clear()
+            For Each item As ListItem In chcItemsCopy
+                CheckBoxList1.Items.Add(item)
+            Next
+        End If
+
+    End Sub
+End Class
